35aba95655994de9cbead30dcf4768d0be4d51b2
Andrei Marinescu <161637088+Andrei-Marinescu@users.noreply.github.com>
2026-04-08 09:33:01 +0300

    v3

--- PrelucrareGrafica/PrelucrareGrafica/main.cpp
+++ PrelucrareGrafica/PrelucrareGrafica/main.cpp
@@ -15,8 +15,15 @@ const float BOX_SIZE = 250.0f;
 float cameraPosX = 0.0f, cameraPosY = 15.0f, cameraPosZ = 45.0f;
 float cameraFrontX = 0.0f, cameraFrontY = -0.2f, cameraFrontZ = -1.0f;
 float cameraUpX = 0.0f, cameraUpY = 1.0f, cameraUpZ = 0.0f;
-float yaw = -90.0f;
-float pitch = -10.0f;
+float yaw = -90.0f; //stanga - dreapta
+float pitch = -10.0f; // sus-jos
+
+
+bool keys[256] = { false };
+
+GLfloat sunLightPos[] = { 0.0f, 150.0f, 0.0f, 0.0f }; 
+float lamp1X = 20.0f, lamp1Z = 30.0f;
+float lamp2X = -50.0f, lamp2Z = -40.0f;
 
 float getTerrainHeight(float x, float z) {
     float dist = sqrt(x * x + z * z);
@@ -71,48 +78,37 @@ GLuint loadTexture(const char* filename) {
     return texID;
 }
 
-void drawSceneCube() {
+void drawSkybox() {
+    glDisable(GL_LIGHTING);
     glEnable(GL_TEXTURE_2D);
-    glColor3f(1.0f, 1.0f, 1.0f);
-
-    // Iarba
-    glBindTexture(GL_TEXTURE_2D, grassTexture);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0.0f, 0.0f);   glVertex3f(-BOX_SIZE, 0.0f, -BOX_SIZE);
-    glTexCoord2f(15.0f, 0.0f);  glVertex3f(BOX_SIZE, 0.0f, -BOX_SIZE);
-    glTexCoord2f(15.0f, 15.0f); glVertex3f(BOX_SIZE, 0.0f, BOX_SIZE);
-    glTexCoord2f(0.0f, 15.0f);  glVertex3f(-BOX_SIZE, 0.0f, BOX_SIZE);
-    glEnd();
+    // Culori mai închise pentru textura de cer, ca să pară noapte
+    glColor3f(0.15f, 0.15f, 0.25f);
 
     glBindTexture(GL_TEXTURE_2D, skyTexture);
     glBegin(GL_QUADS);
-    // Spate
     glTexCoord2f(0.25f, 0.333f); glVertex3f(-BOX_SIZE, 0.0f, -BOX_SIZE);
     glTexCoord2f(0.50f, 0.333f); glVertex3f(BOX_SIZE, 0.0f, -BOX_SIZE);
     glTexCoord2f(0.50f, 0.666f); glVertex3f(BOX_SIZE, BOX_SIZE, -BOX_SIZE);
     glTexCoord2f(0.25f, 0.666f); glVertex3f(-BOX_SIZE, BOX_SIZE, -BOX_SIZE);
-    // Dreapta
     glTexCoord2f(0.50f, 0.333f); glVertex3f(BOX_SIZE, 0.0f, -BOX_SIZE);
     glTexCoord2f(0.75f, 0.333f); glVertex3f(BOX_SIZE, 0.0f, BOX_SIZE);
     glTexCoord2f(0.75f, 0.666f); glVertex3f(BOX_SIZE, BOX_SIZE, BOX_SIZE);
     glTexCoord2f(0.50f, 0.666f); glVertex3f(BOX_SIZE, BOX_SIZE, -BOX_SIZE);
-    // Fata
     glTexCoord2f(0.75f, 0.333f); glVertex3f(BOX_SIZE, 0.0f, BOX_SIZE);
     glTexCoord2f(1.00f, 0.333f); glVertex3f(-BOX_SIZE, 0.0f, BOX_SIZE);
     glTexCoord2f(1.00f, 0.666f); glVertex3f(-BOX_SIZE, BOX_SIZE, BOX_SIZE);
     glTexCoord2f(0.75f, 0.666f); glVertex3f(BOX_SIZE, BOX_SIZE, BOX_SIZE);
-    // Stanga
     glTexCoord2f(0.00f, 0.333f); glVertex3f(-BOX_SIZE, 0.0f, BOX_SIZE);
     glTexCoord2f(0.25f, 0.333f); glVertex3f(-BOX_SIZE, 0.0f, -BOX_SIZE);
     glTexCoord2f(0.25f, 0.666f); glVertex3f(-BOX_SIZE, BOX_SIZE, -BOX_SIZE);
     glTexCoord2f(0.00f, 0.666f); glVertex3f(-BOX_SIZE, BOX_SIZE, BOX_SIZE);
-    // Sus
     glTexCoord2f(0.25f, 0.666f); glVertex3f(-BOX_SIZE, BOX_SIZE, -BOX_SIZE);
     glTexCoord2f(0.25f, 1.000f); glVertex3f(-BOX_SIZE, BOX_SIZE, BOX_SIZE);
     glTexCoord2f(0.50f, 1.000f); glVertex3f(BOX_SIZE, BOX_SIZE, BOX_SIZE);
     glTexCoord2f(0.50f, 0.666f); glVertex3f(BOX_SIZE, BOX_SIZE, -BOX_SIZE);
     glEnd();
-    glDisable(GL_TEXTURE_2D);
+
+    glEnable(GL_LIGHTING);
 }
 
 void drawTerrain() {
@@ -122,6 +118,8 @@ void drawTerrain() {
 
     const float STEP = 4.0f;
     const float MAX_SIZE = BOX_SIZE;
+
+    glNormal3f(0.0f, 1.0f, 0.0f);
 
     for (float z = -MAX_SIZE; z < MAX_SIZE; z += STEP) {
         glBegin(GL_TRIANGLE_STRIP);
@@ -136,7 +134,6 @@ void drawTerrain() {
         }
         glEnd();
     }
-    glDisable(GL_TEXTURE_2D);
 }
 
 void drawTrack() {
@@ -148,8 +145,9 @@ void drawTrack() {
     float radiusZ = 60.0f;
     float trackWidth = 10.0f;
     int segments = 80;
-
     float texRepeat = 0.5f;
+
+    glNormal3f(0.0f, 1.0f, 0.0f);
 
     glBegin(GL_QUAD_STRIP);
     for (int i = 0; i <= segments; ++i) {
@@ -172,15 +170,16 @@ void drawTrack() {
         glVertex3f(x2, y2, z2);
     }
     glEnd();
-    glDisable(GL_TEXTURE_2D);
 }
 
-void drawBuilding(float x, float z, float width, float height, float depth) {
+void drawBuilding(float x, float z, float width, float height, float depth, bool isShadow = false) {
     float y = getTerrainHeight(x, z);
 
-    glEnable(GL_TEXTURE_2D);
-    glBindTexture(GL_TEXTURE_2D, buildingTexture);
-    glColor3f(1.0f, 1.0f, 1.0f);
+    if (!isShadow) {
+        glEnable(GL_TEXTURE_2D);
+        glBindTexture(GL_TEXTURE_2D, buildingTexture);
+        glColor3f(1.0f, 1.0f, 1.0f);
+    }
 
     float halfWidth = width / 2.0f;
     float halfDepth = depth / 2.0f;
@@ -190,60 +189,61 @@ void drawBuilding(float x, float z, float width, float height, float depth) {
     float texRepY = height / 10.0f;
 
     glBegin(GL_QUADS);
-
-    // fata
+    glNormal3f(0.0f, 0.0f, 1.0f);
     glTexCoord2f(0.0f, 0.0f);       glVertex3f(x - halfWidth, y, z + halfDepth);
     glTexCoord2f(texRepX, 0.0f);    glVertex3f(x + halfWidth, y, z + halfDepth);
     glTexCoord2f(texRepX, texRepY); glVertex3f(x + halfWidth, topY, z + halfDepth);
     glTexCoord2f(0.0f, texRepY);    glVertex3f(x - halfWidth, topY, z + halfDepth);
 
-    // spate
+    glNormal3f(0.0f, 0.0f, -1.0f);
     glTexCoord2f(0.0f, 0.0f);       glVertex3f(x + halfWidth, y, z - halfDepth);
     glTexCoord2f(texRepX, 0.0f);    glVertex3f(x - halfWidth, y, z - halfDepth);
     glTexCoord2f(texRepX, texRepY); glVertex3f(x - halfWidth, topY, z - halfDepth);
     glTexCoord2f(0.0f, texRepY);    glVertex3f(x + halfWidth, topY, z - halfDepth);
 
-    // stanga
+    glNormal3f(-1.0f, 0.0f, 0.0f);
     glTexCoord2f(0.0f, 0.0f);       glVertex3f(x - halfWidth, y, z - halfDepth);
     glTexCoord2f(texRepX, 0.0f);    glVertex3f(x - halfWidth, y, z + halfDepth);
     glTexCoord2f(texRepX, texRepY); glVertex3f(x - halfWidth, topY, z + halfDepth);
     glTexCoord2f(0.0f, texRepY);    glVertex3f(x - halfWidth, topY, z - halfDepth);
 
-    // dreapta
+    glNormal3f(1.0f, 0.0f, 0.0f);
     glTexCoord2f(0.0f, 0.0f);       glVertex3f(x + halfWidth, y, z + halfDepth);
     glTexCoord2f(texRepX, 0.0f);    glVertex3f(x + halfWidth, y, z - halfDepth);
     glTexCoord2f(texRepX, texRepY); glVertex3f(x + halfWidth, topY, z - halfDepth);
     glTexCoord2f(0.0f, texRepY);    glVertex3f(x + halfWidth, topY, z + halfDepth);
 
-    // sus
+    glNormal3f(0.0f, 1.0f, 0.0f);
     glTexCoord2f(0.0f, 0.0f);       glVertex3f(x - halfWidth, topY, z - halfDepth);
     glTexCoord2f(texRepX, 0.0f);    glVertex3f(x + halfWidth, topY, z - halfDepth);
     glTexCoord2f(texRepX, texRepX); glVertex3f(x + halfWidth, topY, z + halfDepth);
     glTexCoord2f(0.0f, texRepX);    glVertex3f(x - halfWidth, topY, z + halfDepth);
-
     glEnd();
-    glDisable(GL_TEXTURE_2D);
+
+    if (!isShadow) {
+        glDisable(GL_TEXTURE_2D);
+    }
 }
 
-void drawTree(float x, float z) {
+void drawTree(float x, float z, bool isShadow = false) {
     float y = getTerrainHeight(x, z);
 
     glDisable(GL_TEXTURE_2D);
     glPushMatrix();
     glTranslatef(x, y, z);
 
-    glColor3f(0.55f, 0.27f, 0.07f);
+    if (!isShadow) glColor3f(0.55f, 0.27f, 0.07f);
     glPushMatrix();
     glRotatef(-90.0f, 1.0f, 0.0f, 0.0f);
     glutSolidCone(1.5, 6.0, 10, 2);
     glPopMatrix();
 
-    glColor3f(0.1f, 0.5f, 0.1f);
+    if (!isShadow) glColor3f(0.1f, 0.5f, 0.1f);
     glTranslatef(0.0f, 5.0f, 0.0f);
     glutSolidSphere(4.0, 12, 12);
 
     glPopMatrix();
-    glColor3f(1.0f, 1.0f, 1.0f);
+    if (!isShadow) glColor3f(1.0f, 1.0f, 1.0f);
 }
 
 void drawMountain() {
@@ -253,7 +253,6 @@ void drawMountain() {
 
     float baseX = 0.0f;
     float baseZ = 0.0f;
-
     float height = 30.0f;
     float baseWidth = 40.0f;
     float halfWidth = baseWidth / 2.0f;
@@ -268,45 +267,106 @@ void drawMountain() {
     float v4x = baseX - halfWidth; float v4z = baseZ - halfWidth;
 
     glBegin(GL_TRIANGLES);
-    // Fata 1
+    glNormal3f(0.0f, 0.7f, 0.7f);
     glTexCoord2f(1.0f, 2.0f); glVertex3f(peakX, peakY, peakZ);
     glTexCoord2f(0.0f, 0.0f); glVertex3f(v1x, getTerrainHeight(v1x, v1z), v1z);
     glTexCoord2f(2.0f, 0.0f); glVertex3f(v2x, getTerrainHeight(v2x, v2z), v2z);
-    // Fata 2
+
+    glNormal3f(0.7f, 0.7f, 0.0f);
     glTexCoord2f(1.0f, 2.0f); glVertex3f(peakX, peakY, peakZ);
     glTexCoord2f(0.0f, 0.0f); glVertex3f(v2x, getTerrainHeight(v2x, v2z), v2z);
     glTexCoord2f(2.0f, 0.0f); glVertex3f(v3x, getTerrainHeight(v3x, v3z), v3z);
-    // Fata 3
+
+    glNormal3f(0.0f, 0.7f, -0.7f);
     glTexCoord2f(1.0f, 2.0f); glVertex3f(peakX, peakY, peakZ);
     glTexCoord2f(0.0f, 0.0f); glVertex3f(v3x, getTerrainHeight(v3x, v3z), v3z);
     glTexCoord2f(2.0f, 0.0f); glVertex3f(v4x, getTerrainHeight(v4x, v4z), v4z);
-    // Fata 4
+
+    glNormal3f(-0.7f, 0.7f, 0.0f);
     glTexCoord2f(1.0f, 2.0f); glVertex3f(peakX, peakY, peakZ);
     glTexCoord2f(0.0f, 0.0f); glVertex3f(v4x, getTerrainHeight(v4x, v4z), v4z);
     glTexCoord2f(2.0f, 0.0f); glVertex3f(v1x, getTerrainHeight(v1x, v1z), v1z);
-
     glEnd();
+
     glDisable(GL_TEXTURE_2D);
 }
 
-void drawStaticObjects() {
-    drawTree(-80.0f, 0.0f);
-    drawTree(80.0f, 50.0f);
-    drawTree(-50.0f, -90.0f);
-    drawTree(100.0f, -80.0f);
-    drawTree(0.0f, 100.0f);
+void drawStaticObjects(bool isShadow = false) {
+    drawTree(-80.0f, 0.0f, isShadow);
+    drawTree(80.0f, 50.0f, isShadow);
+    drawTree(-50.0f, -90.0f, isShadow);
+    drawTree(100.0f, -80.0f, isShadow);
+    drawTree(0.0f, 100.0f, isShadow);
 
-    drawBuilding(90.0f, 0.0f, 20.0f, 40.0f, 20.0f);
-    drawBuilding(-90.0f, 60.0f, 25.0f, 50.0f, 25.0f);
-    drawBuilding(0.0f, -110.0f, 30.0f, 35.0f, 30.0f);
-    drawBuilding(120.0f, 100.0f, 20.0f, 60.0f, 20.0f);
-    drawBuilding(-120.0f, -100.0f, 35.0f, 45.0f, 35.0f);
+    drawBuilding(90.0f, 0.0f, 20.0f, 40.0f, 20.0f, isShadow);
+    drawBuilding(-90.0f, 60.0f, 25.0f, 50.0f, 25.0f, isShadow);
+    drawBuilding(0.0f, -110.0f, 30.0f, 35.0f, 30.0f, isShadow);
+    drawBuilding(120.0f, 100.0f, 20.0f, 60.0f, 20.0f, isShadow);
+    drawBuilding(-120.0f, -100.0f, 35.0f, 45.0f, 35.0f, isShadow);
+    drawBuilding(-100.0f, -40.0f, 15.0f, 55.0f, 15.0f, isShadow);
+    drawBuilding(70.0f, -100.0f, 40.0f, 25.0f, 20.0f, isShadow);
+    drawBuilding(110.0f, -30.0f, 20.0f, 70.0f, 20.0f, isShadow);
+    drawBuilding(-60.0f, 120.0f, 25.0f, 30.0f, 25.0f, isShadow);
+    drawBuilding(40.0f, 130.0f, 30.0f, 40.0f, 30.0f, isShadow);
+}
 
-    drawBuilding(-100.0f, -40.0f, 15.0f, 55.0f, 15.0f); 
-    drawBuilding(70.0f, -100.0f, 40.0f, 25.0f, 20.0f);  
-    drawBuilding(110.0f, -30.0f, 20.0f, 70.0f, 20.0f);  
-    drawBuilding(-60.0f, 120.0f, 25.0f, 30.0f, 25.0f); 
-    drawBuilding(40.0f, 130.0f, 30.0f, 40.0f, 30.0f); 
+void drawLamppost(float x, float z, int lightID) {
+    float y = getTerrainHeight(x, z);
+    float height = 20.0f;
+    glDisable(GL_TEXTURE_2D);
+
+    glDisable(GL_LIGHTING);
+    glColor3f(0.2f, 0.2f, 0.2f);
+    glPushMatrix();
+    glTranslatef(x, y + height / 2.0f, z);
+    glScalef(1.0f, height, 1.0f);
+    glutSolidCube(1.0);
+    glPopMatrix();
+
+    glColor3f(1.0f, 1.0f, 0.6f);
+    glPushMatrix();
+    glTranslatef(x, y + height, z);
+    glutSolidSphere(2.0, 10, 10);
+    glPopMatrix();
+    glEnable(GL_LIGHTING);
+
+    GLfloat lightPos[] = { x, y + height - 2.0f, z, 1.0f };
+    GLfloat diffLight[] = { 0.8f, 0.8f, 0.5f, 1.0f };
+    GLfloat linearAttenuation = 0.01f;
+
+    glLightfv(GL_LIGHT0 + lightID, GL_POSITION, lightPos);
+    glLightfv(GL_LIGHT0 + lightID, GL_DIFFUSE, diffLight);
+    glLightf(GL_LIGHT0 + lightID, GL_LINEAR_ATTENUATION, linearAttenuation);
+    glEnable(GL_LIGHT0 + lightID);
+}
+
+void drawShadow(float lx, float ly, float lz, float lw) {
+    glDisable(GL_LIGHTING);
+    glDisable(GL_TEXTURE_2D);
+
+    // pentru transparenta umbrei
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    // Umbra cu opacitate 70%
+    glColor4f(0.0f, 0.0f, 0.0f, 0.7f); 
+
+    glPushMatrix();
+    glTranslatef(0.0f, 0.6f, 0.0f);
+
+    float shadowMat[16] = {
+        ly,   0,   0,   0,
+       -lx,   0, -lz, -lw,
+         0,   0,  ly,   0,
+         0,   0,   0,  ly
+    };
+    glMultMatrixf(shadowMat);
+
+    drawStaticObjects(true);
+
+    glPopMatrix();
+
+    glDisable(GL_BLEND);
+    glEnable(GL_LIGHTING);
 }
 
 void handleMouseMove(int xpos, int ypos) {
@@ -316,7 +376,7 @@ void handleMouseMove(int xpos, int ypos) {
 
     float xoffset = (float)(xpos - centerX);
     float yoffset = (float)(centerY - ypos);
-    float sensitivity = 0.1f;
+    float sensitivity = 0.15f;
 
     yaw += xoffset * sensitivity;
     pitch += yoffset * sensitivity;
@@ -334,22 +394,23 @@ void handleMouseMove(int xpos, int ypos) {
     cameraFrontZ = frontZ / length;
 
     glutWarpPointer(centerX, centerY);
-    glutPostRedisplay();
 }
 
-void handleKeyboard(unsigned char key, int x, int y) {
-    float cameraSpeed = 1.0f;
-    if (key == 'w' || key == 'W') {
+
+void updateCamera() {
+    float cameraSpeed = 0.5f;
+
+    if (keys['w'] || keys['W']) {
         cameraPosX += cameraSpeed * cameraFrontX;
         cameraPosY += cameraSpeed * cameraFrontY;
         cameraPosZ += cameraSpeed * cameraFrontZ;
     }
-    if (key == 's' || key == 'S') {
+    if (keys['s'] || keys['S']) {
         cameraPosX -= cameraSpeed * cameraFrontX;
         cameraPosY -= cameraSpeed * cameraFrontY;
         cameraPosZ -= cameraSpeed * cameraFrontZ;
     }
-    if (key == 'a' || key == 'A') {
+    if (keys['a'] || keys['A']) {
         float rightX = cameraFrontY * cameraUpZ - cameraFrontZ * cameraUpY;
         float rightY = cameraFrontZ * cameraUpX - cameraFrontX * cameraUpZ;
         float rightZ = cameraFrontX * cameraUpY - cameraFrontY * cameraUpX;
@@ -358,7 +419,7 @@ void handleKeyboard(unsigned char key, int x, int y) {
         cameraPosY -= (rightY / length) * cameraSpeed;
         cameraPosZ -= (rightZ / length) * cameraSpeed;
     }
-    if (key == 'd' || key == 'D') {
+    if (keys['d'] || keys['D']) {
         float rightX = cameraFrontY * cameraUpZ - cameraFrontZ * cameraUpY;
         float rightY = cameraFrontZ * cameraUpX - cameraFrontX * cameraUpZ;
         float rightZ = cameraFrontX * cameraUpY - cameraFrontY * cameraUpX;
@@ -367,7 +428,22 @@ void handleKeyboard(unsigned char key, int x, int y) {
         cameraPosY += (rightY / length) * cameraSpeed;
         cameraPosZ += (rightZ / length) * cameraSpeed;
     }
+
+    if (keys['e'] || keys['E']) cameraPosY += cameraSpeed;
+    if (keys['q'] || keys['Q']) cameraPosY -= cameraSpeed;
+}
+
+void handleKeyboard(unsigned char key, int x, int y) {
+    keys[key] = true;
     if (key == 27) exit(0);
+}
+
+void handleKeyboardUp(unsigned char key, int x, int y) {
+    keys[key] = false;
+}
+
+void idle() {
+    updateCamera();
     glutPostRedisplay();
 }
 
@@ -379,18 +455,42 @@ void renderScene() {
         cameraPosX + cameraFrontX, cameraPosY + cameraFrontY, cameraPosZ + cameraFrontZ,
         cameraUpX, cameraUpY, cameraUpZ);
 
-    drawSceneCube();
+    drawSkybox();
+
+    glLightfv(GL_LIGHT0, GL_POSITION, sunLightPos);
+
     drawTerrain();
     drawTrack();
     drawMountain();
-    drawStaticObjects();
+
+    drawLamppost(lamp1X, lamp1Z, 1);
+    drawLamppost(lamp2X, lamp2Z, 2);
+
+    drawStaticObjects(false);
+
+    drawShadow(sunLightPos[0], sunLightPos[1], sunLightPos[2], sunLightPos[3]);
+    drawShadow(lamp1X, getTerrainHeight(lamp1X, lamp1Z) + 20.0f, lamp1Z, 1.0f);
+    drawShadow(lamp2X, getTerrainHeight(lamp2X, lamp2Z) + 20.0f, lamp2Z, 1.0f);
 
     glutSwapBuffers();
 }
 
 void init() {
-    glClearColor(0.6f, 0.7f, 0.8f, 1.0f);
+    glClearColor(0.02f, 0.02f, 0.05f, 1.0f);
     glEnable(GL_DEPTH_TEST);
+
+    glEnable(GL_LIGHTING);
+    glEnable(GL_NORMALIZE);
+
+    GLfloat ambientLight[] = { 0.05f, 0.05f, 0.08f, 1.0f };
+    GLfloat diffuseLight[] = { 0.15f, 0.15f, 0.25f, 1.0f };
+
+    glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLight);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLight);
+    glEnable(GL_LIGHT0);
+
+    glEnable(GL_COLOR_MATERIAL);
+    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
 
     grassTexture = loadTexture("grass1.bmp");
     mountainTexture = loadTexture("stone.jpg");
@@ -411,7 +511,7 @@ void handleResize(int w, int h) {
 
 int main(int argc, char** argv) {
     glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH | GLUT_ALPHA);
     glutInitWindowSize(1024, 768);
     glutCreateWindow("Game");
 
@@ -422,7 +522,10 @@ int main(int argc, char** argv) {
 
     glutSetCursor(GLUT_CURSOR_NONE);
     glutPassiveMotionFunc(handleMouseMove);
+
     glutKeyboardFunc(handleKeyboard);
+    glutKeyboardUpFunc(handleKeyboardUp); 
+    glutIdleFunc(idle);                 
 
     glutMainLoop();
     return 0;
